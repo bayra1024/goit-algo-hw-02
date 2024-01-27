3952f20e16d92ceee95561acda4b47b2584f5459
bayra1024 <bayra1024@gmail.com>
2024-01-28 01:38:07 +0200

    Upd_next

--- task_1.py
+++ task_1.py
@@ -8,7 +8,7 @@ request_queue = queue.Queue()
 
 def generate_request():
     request_id = str(uuid.uuid4())
-    request_data = {f"Request {request_id}"}
+    request_data = {"request_id": request_id, "content": f"Request {request_id}"}
     request_queue.put(request_data)
     print(f"Generated request: {request_data}")
 
@@ -21,7 +21,10 @@ def process_request():
         print("Queue is empty. No requests to process.")
 
 
-while True:
-    generate_request()
-    process_request()
-    time.sleep(1)
+try:
+    while True:
+        generate_request()
+        process_request()
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Program terminated by user.")
